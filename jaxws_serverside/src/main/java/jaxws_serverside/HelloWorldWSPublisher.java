5ba30412c4b9e8038b6994b0326b299ce2eb68aa
thomas <thomas@thomas-eMachines-E725>
2016-09-27 23:10:14 +0100

    first commit

---
+++ jaxws_serverside/src/main/java/jaxws_serverside/HelloWorldWSPublisher.java
@@ -0,0 +1,8 @@
+package jaxws_serverside;
+
+import javax.xml.ws.Endpoint; 
+public class HelloWorldWSPublisher { 
+	public static void main(String[] args) { 
+		Endpoint.publish("http://localhost:8080/WS/HelloWorld",new HelloWorldImpl()); 
+	}
+}
